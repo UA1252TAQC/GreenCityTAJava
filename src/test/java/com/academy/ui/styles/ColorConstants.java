54c5a156d85b57dfd87dcc0b9ae96927dbc5d584
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-09-13 01:17:03 +0300

    add color

--- src/test/java/com/academy/ui/styles/ColorConstants.java
+++ src/test/java/com/academy/ui/styles/ColorConstants.java
@@ -1,4 +1,16 @@
 package com.academy.ui.styles;
 
 public enum ColorConstants {
+
+    PRIMARYGREEN("#13aa57");
+
+    private final String hex;
+
+    ColorConstants(String hex) {
+        this.hex = hex;
+    }
+
+    public String getHex() {
+        return this.hex;
+    }
 }
