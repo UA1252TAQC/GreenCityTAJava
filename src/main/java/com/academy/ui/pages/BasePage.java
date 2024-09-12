37ee83f18eb9bff279c30506d93bd55b8a8648ab
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-09-12 14:47:38 +0300

    move header to basePage

--- src/main/java/com/academy/ui/pages/BasePage.java
+++ src/main/java/com/academy/ui/pages/BasePage.java
@@ -1,13 +1,17 @@
 package com.academy.ui.pages;
 
 import com.academy.ui.Base;
+import com.academy.ui.components.HeaderComponent;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
 public class BasePage extends Base {
+    public final HeaderComponent headerComponent;
+
     public BasePage(WebDriver driver) {
         super(driver);
         PageFactory.initElements(driver, this);
+        headerComponent = new HeaderComponent(driver, findElement(".//header[@role='banner']"));
     }
 
     public void openURL(String url){
--- src/main/java/com/academy/ui/pages/HomePage.java
+++ src/main/java/com/academy/ui/pages/HomePage.java
@@ -1,19 +1,21 @@
 package com.academy.ui.pages;
 
+import com.academy.ui.components.SignInComponent;
 import org.openqa.selenium.WebDriver;
-import com.academy.ui.components.HeaderComponent;
 import com.academy.ui.components.RegistrationComponent;
 
 public class HomePage extends BasePage {
-    private final HeaderComponent headerComponent;
 
     public HomePage(WebDriver driver) {
         super(driver);
-        headerComponent = new HeaderComponent(driver, findElement(".//header[@role='banner']"));
     }
 
     public RegistrationComponent openRegistrationFormInHeader() {
         return headerComponent.openRegistrationForm();
+    }
+
+    public SignInComponent openSignInFormInHeader() {
+        return headerComponent.openSignInForm();
     }
 
     public HomePage setLanguage(String language) {
