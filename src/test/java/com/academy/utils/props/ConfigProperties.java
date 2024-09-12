dc10708f1f8aa69d8ec3c4e9e3ff757dd22df043
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-09-13 01:16:23 +0300

    add methods to config properties

--- src/test/java/com/academy/utils/props/ConfigProperties.java
+++ src/test/java/com/academy/utils/props/ConfigProperties.java
@@ -9,6 +9,14 @@ public class ConfigProperties extends BaseProperties {
         return properties.getProperty("base.url");
     }
 
+    public String getUsername() {
+        return properties.getProperty("valid.username");
+    }
+
+    public String getPassword() {
+        return properties.getProperty("valid.password");
+    }
+
     public String getEmailToken() {
         return properties.getProperty("email.token");
     }
