6b657835dcb5e6438482f17384e1d45644e11687
Oksana Filipchuk <filipchuk.oksana@gmail.com>
2024-09-13 02:01:25 +0300

    change color constants

--- src/test/java/com/academy/ui/styles/ColorConstants.java
+++ src/test/java/com/academy/ui/styles/ColorConstants.java
@@ -1,16 +1,6 @@
 package com.academy.ui.styles;
 
-public enum ColorConstants {
-
-    PRIMARYGREEN("#13aa57");
-
-    private final String hex;
-
-    ColorConstants(String hex) {
-        this.hex = hex;
-    }
-
-    public String getHex() {
-        return this.hex;
-    }
+public interface ColorConstants {
+    String PRIMARY_GREEN = "#13aa57";
+    String PRIMARY_WHITE = "#fff";
 }
