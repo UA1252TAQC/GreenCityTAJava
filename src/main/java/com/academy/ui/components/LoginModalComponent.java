f78056ee67ada2834cb22d7efcbe59de835dac37
Oyne <andreoyy.litvinovv@gmail.com>
2024-09-17 16:49:47 +0300

    Conflict fix

--- src/main/java/com/academy/ui/components/LoginModalComponent.java
+++ src/main/java/com/academy/ui/components/LoginModalComponent.java
@@ -3,6 +3,7 @@ package com.academy.ui.components;
 import com.academy.ui.components.sub.form.EmailField;
 import com.academy.ui.components.sub.form.PasswordField;
 import com.academy.ui.pages.greenCity.ProfilePage;
+import lombok.Getter;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -12,8 +13,10 @@ public class LoginModalComponent extends BaseComponent {
     @FindBy(xpath = ".//button[@type='submit']")
     protected WebElement signInButton;
 
-    private final EmailField emailField;
-    private final PasswordField passwordField;
+    @Getter
+    private EmailField emailField;
+    @Getter
+    private PasswordField passwordField;
 
     public LoginModalComponent(WebDriver driver, WebElement rootElement) {
         super(driver, rootElement);
@@ -45,7 +48,6 @@ public class LoginModalComponent extends BaseComponent {
 
     public LoginModalComponent clickSignInButton() {
         click(signInButton);
-        sleep(1);
         return this;
     }
 
