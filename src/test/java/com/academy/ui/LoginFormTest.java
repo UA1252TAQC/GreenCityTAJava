25e5cf00562a79db36c3d51d1cd8a2a74765170b
FOleks <alexsasha878794@gmail.com>
2024-09-24 14:43:36 +0300

    rename tests

--- src/test/java/com/academy/ui/LoginFormTest.java
+++ src/test/java/com/academy/ui/LoginFormTest.java
@@ -220,7 +220,7 @@ public class LoginFormTest extends TestRunnerMethodInitDriverHomePage {
     }
 
     @Test
-    public void checkSignInBtnBecomesGreenByValidCreds() {
+    public void verifySignInBtnBecomesGreenByValidCreds() {
         LoginModalComponent logInModalComponent = page.getHeaderComponent()
                 .openLoginForm();
         softAssert.assertFalse(logInModalComponent.isSignInButtonActive());
@@ -266,7 +266,7 @@ public class LoginFormTest extends TestRunnerMethodInitDriverHomePage {
     }
 
     @Test
-    public void verifySignInBtnIsEmptyByEmptyFields() {
+    public void verifySignInBtnIsInactiveByEmptyFields() {
         LoginModalComponent logInModalComponent = page.getHeaderComponent()
                 .openLoginForm();
         boolean isEmailEmpty = logInModalComponent.getEmailField().isEmailFieldEmpty();
